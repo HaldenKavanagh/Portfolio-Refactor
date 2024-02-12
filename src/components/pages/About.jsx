c1db1a81e4f6cca02cc3cfef3a8718ae1cf27f0a
Halden Kavanagh <haldenkav@gmail.com>
2024-02-12 00:36:18 -0700

    added styling to about page and tweaks to projects page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,24 @@
+import "../../styles/About.css";
+
 export default function About() {
   return (
-    <div>
-      <h1>about</h1>
-      <ul>
-        <li>paragraph about me</li>
-        <li>grid of all the technologies i know</li>
+    <div className="about">
+      <h1 className="about-section-title">ABOUT ME</h1>
+      <p className="about-p">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quaerat nobis facilis illum adipisci facere voluptatem laboriosam possimus veritatis fugiat. Iure qui blanditiis debitis, provident molestias neque harum tenetur nisi ab excepturi at quisquam veniam libero, dicta obcaecati adipisci! Accusamus enim reprehenderit tempore eveniet nihil magnam, facilis rem illum voluptatem.</p>
+      
+      <h1 className="about-section-title">SKILLSET</h1>
+      <ul className="skills-ul">
+        <li classname="skills-li">HTML</li>
+        <li classname="skills-li">Git</li>
+        <li classname="skills-li">CSS</li>
+        <li classname="skills-li">JavaScript</li>
+        <li classname="skills-li">Node</li>
+        <li classname="skills-li">Express</li>
+        <li classname="skills-li">sql</li>
+        <li classname="skills-li">PWA</li>
+        <li classname="skills-li">React</li>
+        <li classname="skills-li">MongoDb</li>
+        <li classname="skills-li">Mongoose</li>
       </ul>
     </div>
   );
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -13,85 +13,157 @@ export default function Projects() {
       <div className="container">
         <div className="wrapper">
           <img className="banner-image" src={candleImg}></img>
-          <h1> Star Ray design V1</h1>
-          <p>
+          <h1 className="project-title"> Star Ray design V1</h1>
+          <p className="project-description">
             Lorem ipsum dolor sit amet, <br />
             consectetur adipiscing elit.
           </p>
         </div>
         <div className="button-wrapper">
-          <button className="btn fill">View Github Repo</button>
-          <button className="btn fill">Visit site</button>
+          <a
+            href="https://github.com/scook9/candles-n-things"
+            target="_blank"
+            className="btn fill"
+          >
+            View Github Repo
+          </a>
+          <a
+            href="https://shielded-gorge-64229-3114d756159e.herokuapp.com/"
+            target="_blank"
+            className="btn fill"
+          >
+            Visit site
+          </a>
         </div>
       </div>
       <div className="container">
         <div className="wrapper">
           <img className="banner-image" src={textEditerImg}></img>
-          <h1> PWA text editor</h1>
-          <p>
+          <h1 className="project-title"> PWA text editor</h1>
+          <p className="project-description">
             Lorem ipsum dolor sit amet, <br />
             consectetur adipiscing elit.
           </p>
         </div>
         <div className="button-wrapper">
-          <button className="btn fill">View Github Repo</button>
-          <button className="btn fill">Visit site</button>
+          <a
+            href="https://github.com/HaldenKavanagh/text-editer"
+            target="_blank"
+            className="btn fill"
+          >
+            View Github Repo
+          </a>
+          <a
+            href="https://jate-pwa-text-editor-kavanagh-9dd90f6a05dd.herokuapp.com/"
+            target="_blank"
+            className="btn fill"
+          >
+            Visit site
+          </a>
         </div>
       </div>
       <div className="container">
         <div className="wrapper">
           <img className="banner-image" src={menYouImg}></img>
-          <h1> menYou</h1>
-          <p>
+          <h1 className="project-title"> menYou</h1>
+          <p className="project-description">
             Lorem ipsum dolor sit amet, <br />
             consectetur adipiscing elit.
           </p>
         </div>
         <div className="button-wrapper">
-          <button className="btn fill">View Github Repo</button>
-          <button className="btn fill">Visit site</button>
+          <a
+            href="https://github.com/scook9/menYou"
+            target="_blank"
+            className="btn fill"
+          >
+            View Github Repo
+          </a>
+          <a
+            href="https://scook9.github.io/menYou/"
+            target="_blank"
+            className="btn fill"
+          >
+            Visit site
+          </a>
         </div>
       </div>
       <div className="container">
         <div className="wrapper">
           <img className="banner-image" src={weatherImg}></img>
-          <h1> Weather dashboard</h1>
-          <p>
+          <h1 className="project-title"> Weather dashboard</h1>
+          <p className="project-description">
             Lorem ipsum dolor sit amet, <br />
             consectetur adipiscing elit.
           </p>
         </div>
         <div className="button-wrapper">
-          <button className="btn fill">View Github Repo</button>
-          <button className="btn fill">Visit site</button>
+          <a
+            href="https://github.com/scook9/module-6-weather-dashboard"
+            target="_blank"
+            className="btn fill"
+          >
+            View Github Repo
+          </a>
+          <a
+            href="https://scook9.github.io/module-6-weather-dashboard/"
+            target="_blank"
+            className="btn fill"
+          >
+            Visit site
+          </a>
         </div>
       </div>
       <div className="container">
         <div className="wrapper">
           <img className="banner-image" src={plannerImg}></img>
-          <h1> Workday Planner</h1>
-          <p>
+          <h1 className="project-title"> Workday Planner</h1>
+          <p className="project-description">
             Lorem ipsum dolor sit amet, <br />
             consectetur adipiscing elit.
           </p>
         </div>
         <div className="button-wrapper">
-          <button className="btn fill">View Github Repo</button>
-          <button className="btn fill">Visit site</button>
+          <a
+            href="https://github.com/HaldenKavanagh/Workday-Planner"
+            target="_blank"
+            className="btn fill"
+          >
+            View Github Repo
+          </a>
+          <a
+            href="https://haldenkavanagh.github.io/Workday-Planner/"
+            target="_blank"
+            className="btn fill"
+          >
+            Visit site
+          </a>
         </div>
       </div>
       <div className="container">
         <div className="wrapper">
-          <img className="banner-image" src={quizImg}></img>
-          <h1>Js Quiz</h1>
-          <p>
+          <img className="banner-image" src={quizImg}></img>s
+          <h1 className="project-title">Js Quiz</h1>
+          <p className="project-description">
             Lorem ipsum dolor sit amet, <br />
             consectetur adipiscing elit.
           </p>
         </div>
         <div className="button-wrapper">
-          <button className="btn fill">View Github Repo</button>
-          <button className="btn fill">Visit site</button>
+          <a
+            href="https://github.com/HaldenKavanagh/Js-Quiz"
+            target="_blank"
+            className="btn fill"
+          >
+            View Github Repo
+          </a>
+          <a
+            href="https://haldenkavanagh.github.io/Js-Quiz/"
+            target="_blank"
+            className="btn fill"
+          >
+            Visit site
+          </a>
         </div>
       </div>
     </div>
