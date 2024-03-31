d9eba87f6e1082c02260edde0e128e25d56e3c39
Halden Kavanagh <haldenkav@gmail.com>
2024-03-30 20:49:59 -0600

    animation to navbar

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -9,34 +9,31 @@ import {
   AiOutlineUser,
 } from "react-icons/ai";
 
+import "animate.css";
+
 function NavTabs() {
-  const location = useLocation();
-
-  const getPageName = (pathname) => {
-    // Extract the last part of the pathname as the page name
-    const pageName = pathname.substring(1);
-    return pageName === "" ? "Home" : pageName;
-  };
-
   return (
-    <div className="navbar">
-      <NavLink to="/home" className="nav-link">
+    <div className="navbar ">
+      <NavLink
+        to="/home"
+        className="nav-link animate__animated animate__backInLeft"
+      >
         <AiOutlineHome />
         <span className="nav-text">Home</span>
       </NavLink>
-      <NavLink to="/about" className="nav-link">
+      <NavLink to="/about" className="nav-link animate__animated animate__backInLeft">
         <AiOutlineUser />
         <span className="nav-text">About</span>
       </NavLink>
-      <NavLink to="/projects" className="nav-link">
+      <NavLink to="/projects" className="nav-link animate__animated animate__backInLeft">
         <AiOutlineFundProjectionScreen />
         <span className="nav-text">Projects</span>
       </NavLink>
-      <NavLink to="/resume" className="nav-link">
+      <NavLink to="/resume" className="nav-link animate__animated animate__backInLeft">
         <AiOutlineAudit />
         <span className="nav-text">Resume</span>
       </NavLink>
-      <NavLink to="/contact" className="nav-link">
+      <NavLink to="/contact" className="nav-link animate__animated animate__backInLeft">
         <AiOutlineRocket />
         <span className="nav-text">Contact</span>
       </NavLink>
